1b1d9b040a02dcf558df6f62e4457a801f1ba96d
Stephan Zeissler <stephan.zeissler@moinz.de>
2011-12-19 23:27:47 +0100

    More cleanup of JS code

--- web/js/jquery.wobble.js
+++ web/js/jquery.wobble.js
@@ -1,18 +1,28 @@
-"use strict";
+(function($) {
+	"use strict";
 
-$(document).ready(function() {
+	/**
+	 * Fires a 'window.resize' BUS event, when the window gets resized.
+	 */
+
+	var $window = $(window);
 	function get_size() {
-		return {w: $(window).width(), h: $(window).height()};
+		return {w: $w.width(), h: $w.height()};
 	};
-	var old_size = get_size();
-	BUS.fire('window.resize', {before: old_size, 'to': old_size});
-	$(window).resize(function() {
-		var new_size = get_size();
-		BUS.fire('window.resize', {
-			'before': old_size,
-			'to':  new_size
-		});
-		old_size = new_size;
-	});
-});
+	 
+	$(document).ready(function() {
+		// Fire initial event to setup UI
+		var old_size = get_size();
+		BUS.fire('window.resize', {before: old_size, 'to': old_size});
 
+		// List for changes
+		$w.resize(function() {
+			var new_size = get_size();
+			BUS.fire('window.resize', {
+				'before': old_size,
+				'to':  new_size
+			});
+			old_size = new_size;
+		});
+	});
+})(jQuery);
