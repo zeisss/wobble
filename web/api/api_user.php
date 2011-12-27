2e291caa54481ff3bb7950d7544ef7526278aa3e
Stephan Zeissler <stephan.zeissler@moinz.de>
2011-12-27 12:19:25 +0100

    Prevent the user from adding himself. See #63.

--- web/api/api_user.php
+++ web/api/api_user.php
@@ -102,11 +102,14 @@ function user_add_contact($params) {
 	$contact_email = $params['contact_email'];
 	
 	ValidationService::validate_not_empty($self_user_id);
-	ValidationService::validate_not_empty($contact_email);
+	ValidationService::validate_email($contact_email);
 	
 	$user = UserRepository::getUserByEmail($contact_email);
 
 	if ( $user !== NULL ) {
+		if ($user['id'] == $self_user_id) {
+			return FALSE;
+		}
 		ContactsRepository::addUser($self_user_id, $user['id']);
 		return TRUE;
 	}
