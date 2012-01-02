0ada3db561c458ef43250c635281a564f00f7f7f
Stephan Zeissler <stephan.zeissler@moinz.de>
2012-01-02 16:42:29 +0100

    Say hi to users :)

--- web/js/desktop/DesktopClientHeader.js
+++ web/js/desktop/DesktopClientHeader.js
@@ -3,15 +3,21 @@ function DesktopClientHeader() {
 	this.e = $('<div></div>').attr('id', 'headline').prependTo('body');
 	
 	this.e.append('<div class="navigation">' + 
-			'Moinz.de Wobble | ' +
+			'<span class="userinfo">Moinz.de Wobble</span> | ' +
 			'<a href="http://github.com/zeisss/wobble" target="_new">Source</a> | ' + 
-			'<a href="." id="signout">Sign Out</a>' +
+			'<a href="#" id="signout">Sign Out</a>' +
 			'</div>');
 
+    // 
 	$("#signout").click(function() {
 		API.signout(function(err, data) {
 			if (!err) window.location.reload();
 		});
 		return false;				
 	});
+	
+	// Welcome the user
+	BUS.on('api.user', function() {
+	   $(".navigation .userinfo").text("Hello " + API.user().name);
+	}, this);
 };
