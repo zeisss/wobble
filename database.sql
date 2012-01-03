61c1fd84a6a63dfd753cafc6b3f8a62005fb18e2
Stephan Zeissler <stephan.zeissler@moinz.de>
2012-01-03 11:25:01 +0000

    CUrrent database dump

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.41, for debian-linux-gnu (i486)
+-- MySQL dump 10.13  Distrib 5.1.52, for redhat-linux-gnu (x86_64)
 --
--- Host: localhost    Database: light
+-- Host: localhost    Database: wobble
 -- ------------------------------------------------------
--- Server version	5.1.41-3ubuntu12.10-log
+-- Server version	5.1.52
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,49 +16,101 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `blobs`
+-- Table structure for table `notifications`
 --
 
-DROP TABLE IF EXISTS `blobs`;
+DROP TABLE IF EXISTS `notifications`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `blobs` (
+CREATE TABLE `notifications` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` blob,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=72 DEFAULT CHARSET=latin1;
+  `user_id` int(11) DEFAULT NULL,
+  `data` text,
+  `time` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `i_notifications_user_id` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3063 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `lightnings`
+-- Table structure for table `post_editors`
 --
 
-DROP TABLE IF EXISTS `lightnings`;
+DROP TABLE IF EXISTS `post_editors`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `lightnings` (
-  `lightning_id` int(11) NOT NULL AUTO_INCREMENT,
-  `created_at` int(11) NOT NULL,
-  `owner_user_id` int(11) NOT NULL,
-  `is_private` int(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`lightning_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=273 DEFAULT CHARSET=latin1;
+CREATE TABLE `post_editors` (
+  `topic_id` varchar(255) DEFAULT NULL,
+  `post_id` varchar(255) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  UNIQUE KEY `ui_post_editors` (`topic_id`,`post_id`,`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `lightnings_files`
+-- Table structure for table `post_users_read`
 --
 
-DROP TABLE IF EXISTS `lightnings_files`;
+DROP TABLE IF EXISTS `post_users_read`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `lightnings_files` (
-  `file_id` int(11) NOT NULL AUTO_INCREMENT,
-  `lightning_id` int(11) NOT NULL,
-  `filename` varchar(255) NOT NULL,
-  `content_blob_id` int(11) NOT NULL,
-  PRIMARY KEY (`file_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=372 DEFAULT CHARSET=latin1;
+CREATE TABLE `post_users_read` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `topic_id` varchar(255) NOT NULL,
+  `post_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ui_post_users_read` (`topic_id`,`post_id`,`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=627 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `topic_id` varchar(255) NOT NULL,
+  `post_id` varchar(255) NOT NULL,
+  `content` text,
+  `parent_post_id` varchar(255) DEFAULT NULL,
+  `revision_no` int(11) NOT NULL DEFAULT '1',
+  `created_at` int(11) DEFAULT NULL,
+  `last_touch` int(11) DEFAULT NULL,
+  `deleted` int(11) NOT NULL DEFAULT '0',
+  UNIQUE KEY `ui_posts_id` (`topic_id`,`post_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `topic_readers`
+--
+
+DROP TABLE IF EXISTS `topic_readers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `topic_readers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `topic_id` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ui_topic_readers` (`topic_id`,`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=150 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `topics`
+--
+
+DROP TABLE IF EXISTS `topics`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `topics` (
+  `id` varchar(255) NOT NULL,
+  UNIQUE KEY `topics_id_ui` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,14 +121,32 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `hashed_password` varchar(255) NOT NULL,
-  `plan_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `ui_users_email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=87 DEFAULT CHARSET=latin1;
+  `password_hashed` varchar(255) DEFAULT NULL,
+  `last_touch` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `i_user_name` (`name`),
+  KEY `ui_users_email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users_contacts`
+--
+
+DROP TABLE IF EXISTS `users_contacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `contact_user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `i_contacts_user_id` (`user_id`),
+  KEY `ui_contacts_user_contact` (`user_id`,`contact_user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=141 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -88,4 +158,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-12-18 10:27:16
+-- Dump completed on 2012-01-03 11:24:39
