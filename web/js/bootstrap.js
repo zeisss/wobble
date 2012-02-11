94e18af2b5f3644365fcd5a1467c91d0fb533d36
Stephan Zeissler <stephan.zeissler@moinz.de>
2012-02-11 17:09:28 +0100

    Added script loading to bootstrap.js

--- web/js/bootstrap.js
+++ web/js/bootstrap.js
@@ -1,24 +1,34 @@
-window.WobbleApp = (function() {
-	if (localStorage) {
-		// An enforced app has priority over the UA detection
-		var appName = localStorage.getItem('WOBBLE_ENFORCE_APP');
-		if (appName == "mobile") {
-			return new WobbleMobileClient();
-		} else if(appName == "desktop") {
-			return new WobbleDesktopClient();
-		}
+(function() {
+	var files = [
+		'js/core/cache.js'
+	];
+
+	for(var i = 0; i < files.length; i++) {
+		document.write('<script src="' + files[i] + '"></script>');
 	}
 
-	// If the UA detects a mobile browser, use that
-	var ua = navigator.userAgent;
-	if (ua.match(/android/i) || 
-		ua.match(/iphone/i)) {
-		return new WobbleMobileClient();
-	} 
+	window.WobbleApp = (function() {
+		if (localStorage) {
+			// An enforced app has priority over the UA detection
+			var appName = localStorage.getItem('WOBBLE_ENFORCE_APP');
+			if (appName == "mobile") {
+				return new WobbleMobileClient();
+			} else if(appName == "desktop") {
+				return new WobbleDesktopClient();
+			}
+		}
 
-	// Fallback: Standard desktop version
-	return new WobbleDesktopClient();
+		// If the UA detects a mobile browser, use that
+		var ua = navigator.userAgent;
+		if (ua.match(/android/i) || 
+			ua.match(/iphone/i)) {
+			return new WobbleMobileClient();
+		} 
+
+		// Fallback: Standard desktop version
+		return new WobbleDesktopClient();
+	})();
+	$(document).ready(function() {
+		window.WobbleApp.bootstrap();
+	});
 })();
-$(document).ready(function() {
-	window.WobbleApp.bootstrap();
-});
