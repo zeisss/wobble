dd7d58d321193ab3da8c0fae510d22b17c36d191
Stephan Zeissler <stephan.zeissler@moinz.de>
2012-02-13 21:02:22 +0100

    Fixes the sanitizer

--- web/api/classes/InputSanitizer.class.php
+++ web/api/classes/InputSanitizer.class.php
@@ -13,37 +13,41 @@ class InputSanitizer {
      * - Changes links to open in a new window (target="_new")
      */
     public static function sanitizePostContent($content) {
-        do {
-            $old_content = $content;
-            $content = InputSanitizer::sanitizeLinks($old_content);
-        } while ($content != $old_content);
+        $content = InputSanitizer::sanitizeLinks($content);
 
         return $content;
     }
     
     public static function sanitizeLinks($content) {
-        $i = strpos('<a', $content);
-        if ($i === FALSE) {
-            return $content;
-        }
+        $offset = 0;
         
-        $end = strpos('>', $i);
-        if ($end === FALSE) {
-            return $content;
-        }
-        
-        $j = strpos('target')
-        if ($j === FALSE) {
-            # no target was found. Just replace the starting tag and be done.
-            return substr($content, 0, $i) .
-                   '<a target="_new" ' .
-                   substr($content, $i + 2);
-        }
-        else {
-            # Parsing html is hard. Actually too hard for this little project to do it right here.
-            # The UI only provides a simple without a way to provide a link target. Thus we just
-            # abort here.
-            throw new Exception('Invalid content. Do not use target attribute in links(a): SAN-001');
+        while (true) {
+            $i = strpos($content, '<a', $offset);
+            if ($i === FALSE) {
+                return $content;
+            }    
+            
+            $end = strpos($content, '>', $i);
+            if ($end === FALSE) {
+                return $content;
+            }
+            
+            $j = strpos($content, 'target', $i);
+            if ($j === FALSE) {
+                # no target was found. Just replace the starting tag and be done.
+                $content = substr($content, 0, $i) .
+                           '<a target="_new"' .
+                           substr($content, $i + 2);
+                $offset = $i + 16;
+            }
+            else {
+                # Parsing html is hard. Actually too hard for this little project to do it right here.
+                # The UI only provides a simple without a way to provide a link target. Thus we just
+                # abort here.
+                throw new Exception('Invalid content. Do not use target attribute in links(a): SAN-001');
+            }
         }
     }
 }
+
+print (InputSanitizer::sanitizePostContent('<a href="http://ec2-46-51-143-163.eu-west-1.compute.amazonaws.com/dev/#7-1328992318043">http://ec2-46-51-143-163.eu-west-1.compute.amazonaws.com/dev/#7-1328992318043</a>'));
