4649934bb4d3569305112456ee05aa4277e3a013
Stephan Zeissler <stephan.zeissler@moinz.de>
2012-03-12 09:26:49 +0100

    Added a proper phpunit configuration to automatically run the unit tests.

---
+++ tests/handlers/api_topic_test.php
@@ -0,0 +1,7 @@
+<?php
+require_once dirname(__FILE__) . '/../../web/api/context.php';
+require_once dirname(__FILE__) . '/../../web/api/api_topic.php';
+
+class ApiTopicTest extends PHPUnit_Framework_TestCase {
+  
+}
