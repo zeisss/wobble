1bd9f5ebaf07b5fda7ae30f07adc7e494425ddae
Stephan Zeissler <stephan.zeissler@moinz.de>
2012-05-13 14:17:39 +0200

    Made all functions in NotificationRepository static

--- WobbleApi/model/NotificationRepository.class.php
+++ WobbleApi/model/NotificationRepository.class.php
@@ -3,7 +3,7 @@
  * The NotificationRepository provides a way to store events for a session/user. 
  */
 class NotificationRepository {
-  function push($user_id, $message) {
+  public static function push($user_id, $message) {
     $json = json_encode($message);
 
     $pdo = ctx_getpdo();
@@ -18,7 +18,7 @@ class NotificationRepository {
     $stmt->execute(array($json, $user_id));
   }
 
-  function pushSession($session_id, $message) {
+  public static function pushSession($session_id, $message) {
     $json = json_encode($message);
 
     $pdo = ctx_getpdo();
@@ -33,7 +33,7 @@ class NotificationRepository {
    * Delete all notifications for the given session before the given timestamp. If null
    * is provided for the timestamp, delete all messages.
    */
-  function deleteNotifications($session_id, $timestamp = null) {
+  public static function deleteNotifications($session_id, $timestamp = null) {
     $pdo = ctx_getpdo();
     if (is_null($timestamp)) {
         $stmt = $pdo->prepare('DELETE FROM notifications WHERE session_id = ?');
@@ -47,7 +47,7 @@ class NotificationRepository {
     }
   }
 
-  function getNotifications($session_id, $timestamp) {  
+  public static function getNotifications($session_id, $timestamp) {  
     $pdo = ctx_getpdo();
         
     $stmt = $pdo->prepare('SELECT data FROM notifications WHERE session_id = ? AND time <= ?');
