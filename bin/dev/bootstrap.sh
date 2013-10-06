181cba88e952b6b43b105c1e522eefff0606eac1
Stephan Zeissler <stephan.zeissler@moinz.de>
2013-10-06 11:23:34 +0000

    Removed Rakefile

--- bin/dev/bootstrap.sh
+++ bin/dev/bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -e
+set -x
+
+echo "Installing php dependencies with composer ..."
+composer install
+
 if [ ! -f etc/config.php ]; then
   echo "Copying etc/template.config.php to etc/config.php"
   cat etc/template.config.php |\
