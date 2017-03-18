7aacb6a84d65f791c54c4234aef8b25bf7a0168d
Stephan Zeissler <stephan.zeissler@rewe-digital.com>
2017-03-18 14:12:38 +0100

    make backup script more resilient

--- bin/mysql-backup.sh
+++ bin/mysql-backup.sh
@@ -6,14 +6,14 @@ BACKUP_DIR="$HOME/var/backup/wobble"
 MYSQL_DBS="szeissler-002"
 MYSQLDUMP="/usr/bin/mysqldump"
 
-if [ ! -f "${CONFIG_FILE}" ]; then
+if [ ! -f "${CONFIG_FILE}" ]; then
 	echo "No ${CONFIG_FILE} found." > /dev/stderr
 	exit 1
 fi
 
 
 echo "Writing backup to ${BACKUP_DIR} ..." > /dev/stderr
-[ ! -d $BACKUP_DIR ] && mkdir $BACKUP_DIR
+[ ! -d $BACKUP_DIR ] && mkdir -p $BACKUP_DIR
 
 for db in "${MYSQL_DBS}"; do
   file="${BACKUP_DIR}/${db}-$(date +%Y-%m-%d).sql.gz"
